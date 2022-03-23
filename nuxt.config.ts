8d5496e8177a048cc64d99e0706d5a3df96c6cae
Mike River <mrsilva@yesmkt.com>
2022-03-23 13:58:05 +0000

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,95 @@
+import { defineNuxtConfig } from 'nuxt3'
+import { IntlifyModuleOptions } from '@intlify/nuxt3'
+import UnpluginComponentsVite from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+
+declare module '@nuxt/schema' {
+  interface NuxtConfig {
+    intlify?: IntlifyModuleOptions
+  }
+}
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  // meta
+  meta: {
+    title: 'Nuxt 3 Awesome Starter',
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Nuxt 3 Awesome Starter',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // css
+  css: ['~/assets/sass/vendor.scss', '~/assets/sass/app.scss'],
+
+  // plugins
+  plugins: ['~/plugins/navbar.ts'],
+
+  // build
+  build: {
+    transpile: ['@headlessui/vue'],
+  },
+
+  // build modules
+  buildModules: [
+    'nuxt-windicss',
+    '@nuxtjs/eslint-module',
+    '@pinia/nuxt',
+    'unplugin-icons/nuxt',
+    '@nuxtjs/svg',
+    '@intlify/nuxt3',
+    '@vueuse/nuxt',
+  ],
+
+  // auto import components
+  components: true,
+
+  // vite plugins
+  vite: {
+    plugins: [
+      UnpluginComponentsVite({
+        dts: true,
+        resolvers: [
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+      }),
+    ],
+  },
+
+  // localization - i18n config
+  intlify: {
+    localeDir: 'locales',
+    vueI18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      availableLocales: ['en', 'id', 'ja'],
+    },
+  },
+
+  // vueuse
+  vueuse: {
+    ssrHandlers: true,
+  },
+
+  // windicss
+  windicss: {
+    analyze: {
+      analysis: {
+        interpretUtilities: false,
+      },
+      server: {
+        port: 4000,
+        open: false,
+      },
+    },
+    scan: true,
+  },
+})
---
+++ pages/blank.vue
@@ -0,0 +1,20 @@
+<template>
+  <PageWrapper>
+    <PageHeader>
+      <PageTitle :text="$t('pages.blank.title')" class="capitalize" />
+    </PageHeader>
+    <PageBody>
+      <PageSection>
+        <div v-for="i in 30" :key="i" class="text-6xl uppercase">
+          {{ $t('pages.blank.just_blank_page_with_title') }}
+        </div>
+      </PageSection>
+    </PageBody>
+  </PageWrapper>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'page',
+})
+</script>
